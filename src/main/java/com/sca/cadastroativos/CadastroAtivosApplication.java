fa3fa93e110dee4ea80a17e36cd1303077ae1c51
josesm <jamacedo1@stefanini.com>
2020-05-17 17:05:21 -0300

    Hello World

---
+++ src/main/java/com/sca/cadastroativos/CadastroAtivosApplication.java
@@ -0,0 +1,13 @@
+package com.sca.cadastroativos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAtivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAtivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sca/cadastroativos/CadastroAtivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sca.cadastroativos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAtivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
