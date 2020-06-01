5332f5038ab5f17ccf38948f25ed3b3759e3227d
josesm <jamacedo1@stefanini.com>
2020-05-31 23:14:39 -0300

    swagger-doc

--- src/main/java/com/sca/cadastroativos/HelloController.java
+++ src/main/java/com/sca/cadastroativos/HelloController.java
@@ -1,14 +1,18 @@
 package com.sca.cadastroativos;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greetings2")
 public class HelloController {
 
-	@RequestMapping("/")
+	@GetMapping("")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+	
+	
 
 }
